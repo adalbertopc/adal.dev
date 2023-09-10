ff4398f80772e68bcda66b0964a47b9cfe44250e
Adalberto Prado <jesus.prado@benandfrank.com>
2023-09-10 13:38:29 -0700

    ref: upgrade to astro 3 and delete all files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import image from "@astrojs/image";
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind()],
 });
